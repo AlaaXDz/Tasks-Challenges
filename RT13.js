626c97196d6170a5d2ace3fbd10c71914704b482
chakib <c-a-b@live.fr>
2022-04-12 14:00:21 +0100

    ah yaaa

--- RT13.js
+++ RT13.js
@@ -6,10 +6,16 @@ function rot13(str) {
   newStr=[];
   // The use of for loop and if conditions to loop over every character and check whether it's above N or under it
   for (let i=0; i < arrStr.length ; i++){
-    if (arrStr[i].charCodeAt()>65 && arrStr[i].charCodeAt() < 78){ // the charCodeAt methode to get the ASCI code of the character and compare it to the middle of our shift
+    if (arrStr[i].charCodeAt()>=65 && arrStr[i].charCodeAt() < 78){ // the charCodeAt methode to get the ASCI code of the character and compare it to the middle of our shift
      newStr.splice(i,0,String.fromCharCode(str.charCodeAt(i)+13));
      // newStr+=(String.fromCharCode(str.charCodeAt(i)+13));
     //console.log(newStr);
+    }
+    if ((",.?!").includes(arrStr[i])){
+      newStr.splice(i,0,String.fromCharCode(str.charCodeAt(i)));
+      continue;
+      //newStr+=(String.fromCharCode(str.charCodeAt(i)-13));
+     // console.log(newStr);
     }
     if (arrStr[i].charCodeAt()>77 && arrStr[i].charCodeAt() < 91){
       newStr.splice(i,0,String.fromCharCode(str.charCodeAt(i)-13));
@@ -21,8 +27,7 @@ function rot13(str) {
     }
   }
   let arr=newStr.join('');
+  console.log(arr)
   //console.log(newStr);
-  return console.log(arr);
+  return arr;
 }
-
-rot13("SERR PBQR PNZC");
